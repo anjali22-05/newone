c17073da46af145d232f7bfa439ae394aec833f9
Anjali_Verma <verla003renu@gmail.com>
2026-03-01 23:36:10 +0530

    Add threshold dependency to IntersectionObserver effect

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ function useInView(threshold = 0.1) {
     const obs = new IntersectionObserver(([e]) => { if (e.isIntersecting) setInView(true); }, { threshold });
     if (ref.current) obs.observe(ref.current);
     return () => obs.disconnect();
-  }, []);
+  }, [threshold]);
   return [ref, inView];
 }
 
